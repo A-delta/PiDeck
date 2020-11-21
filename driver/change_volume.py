4f9534fe4cda6ed1d02f4a5829f0a3533f9498bc
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2020-11-21 23:30:37 +0100

    Improve volume changing (Linux)

--- driver/change_volume.py
+++ driver/change_volume.py
@@ -4,6 +4,24 @@ from subprocess import run as cmdrun, PIPE as cmdPIPE
 from math_functions import take_closest
 
 def change_volume_lnx(level, sound_conf):
-    vol = cmdrun(["amixer", "get" ,"Master"], stdout=cmdPIPE)
-    vol = str(vol.stdout).split("[")[1].replace("]", "").replace("%", "")
-    take_closest(sound_conf, vol)
+    current_vol = cmdrun(["amixer", "get" ,"Master"], stdout=cmdPIPE)
+    current_vol = int(str(current_vol.stdout).split("[")[1].replace("]", "").replace("%", ""))
+    current_vol = take_closest(sound_conf, current_vol)
+    wanted_vol = take_closest(sound_conf, int(level))
+    c = 1
+    d = 1
+    k = 0
+    for step in sound_conf:
+        k = k + 1
+        if c == 1:
+            if step == wanted_vol:
+                wanted_step = k
+                c = 0
+        if d == 1:
+            if step == current_vol:
+                actual_step = k
+                d = 0
+    step_diff = wanted_step - actual_step # It means that if `step_diff` is positive, volume must increase of `step_diff` and if `step_diff` is negative, volume must decrease of `abs(step_diff)`
+
+
+change_volume_lnx(0, [0, 0, 15, 31, 43, 52, 60, 66, 71, 76, 79, 84, 87, 90, 93, 95, 99, 100])
