2478df7a5d1414f010cab267367dd628669130f5
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2020-11-22 00:32:48 +0100

    Add request listener support

--- driver/main.py
+++ driver/main.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-def errorclose(reason):
-    print(f'The program encountered a problem and needs to close.\nError : “{reason}”')
+"""def errorclose(reason):
+    print(f'The program encountered a problem and needs to close.\nError : “{reason}”')"""
 
-try:
-    import change_volume as vol
-    import prog_init as init
+#try:
+import change_volume as vol
+import prog_init as init
+from os import system as systex
 
-    if init.check_platorm() == 'linux':
-        pc_init = init.init_lnx
-        pc_init = dict(pc_init)
+if init.check_platorm() == 'linux':
+    pc_init = init.init_lnx()
+    systex("gunicorn --certfile cert.pem --keyfile key.pem --bind 0.0.0.0:9876 wsgi:app")
 
-except:
-    errorclose(reason="Unknown")
+"""except:
+    errorclose(reason="Unknown")"""
