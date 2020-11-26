7d4962b935bc22377232b660d77dcaa456a1ca10
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2020-11-26 23:59:32 +0100

    Add key press functions

---
+++ driver/keyboard_actions.py
@@ -0,0 +1,67 @@
+def press_key(platform, todo, value):
+    if todo == "alphabet":
+        if platform == 'linux':
+            print('Not implemented yet.')
+        else:
+            from keyboard import send as press
+
+            press(value)
+    elif todo == "media":
+        if platform == 'linux':
+            print('Not implemented yet.')
+        else:
+            from keyboard import send as press
+
+            if value == "volup":  
+                press('volume up')
+            elif value == "voldown":
+                press('volume down')
+            elif value ==  "mute":
+                press('volume mute')
+            elif value == "pp":
+                press('play/pause media')
+            elif value == "next":
+                press('next track')
+            elif value == "previous":
+                press('previous track')
+    elif todo == "fn":
+        if platform == 'linux':
+            print('Not implemented yet.')
+        else:
+            from keyboard import send as press
+            
+            press(f'f{value}')
+    elif todo == "other":
+        if platform == "linux":
+            print('Not implemented yet.')
+        else:
+            from keyboard import send as press
+
+            if value == "psc":
+                press('print screen')
+            elif value == "pos1":
+                press('home')
+            elif value == "end":
+                press('end')
+            elif value == "ins":
+                press('insert')
+            elif value == "del":
+                press('delete')
+            elif value == "enter":
+                press('enter')
+            elif value == "backspace":
+                press('backspace')
+            elif value == "tab":
+                press('tab')
+            elif value == "pup":
+                press('page up')
+            elif value == "pdown":
+                press('page down')
+            elif value == "shift":
+                press('shift')
+            elif value == "ctrl":
+                press('ctrl')
+            elif value == "alt":
+                press('alt')
+            elif value == "super":
+                press('left windows')
