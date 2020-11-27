ff031478808749908dc71de81bf1ec35053fcfa4
firmin-launay <firmin.launay@outlook.fr>
2020-11-27 17:15:09 +0100

    Add press_key support for Linux

--- driver/keyboard_actions.py
+++ driver/keyboard_actions.py
@@ -1,13 +1,20 @@
+# -*- coding: utf-8 -*-
+
 def press_key(platform, todo, value):
     if todo == "alphabet":
         if platform == 'linux':
             from os import system as systex
 
             systex(f"xdotool key {value}")
-        else:
+
+        elif platform == 'windows':
             from keyboard import send as press
 
             press(value)
+
+        else:
+            print("Not implemented.")
+
     elif todo == "media":
         if platform == 'linux':
             from os import system as systex
@@ -24,7 +31,8 @@ def press_key(platform, todo, value):
                 systex('xdotool key XF86AudioNext')
             elif value == "previous":
                 systex('xdotool key XF86AudioPrev')
-        else:
+
+        elif platform == 'windows':
             from keyboard import send as press
 
             if value == "volup":  
@@ -39,19 +47,56 @@ def press_key(platform, todo, value):
                 press('next track')
             elif value == "previous":
                 press('previous track')
+
+        else:
+            print('Not implemented.')
+
     elif todo == "fn":
         if platform == 'linux':
             from os import system as systex
             
             systex(f'xdotool key F{value}')
-        else:
+
+        if platform == 'windows':
             from keyboard import send as press
             
             press(f'f{value}')
+
+        else:
+            print('Not implemented.')
+
     elif todo == "other":
         if platform == "linux":
-            print('Not implemented yet.')
-        else:
+            from os import system as systex
+
+            if value == "psc":
+                systex('xdotool key Print')
+            elif value == "pos1":
+                systex('xdotool key Home')
+            elif value == "end":
+                systex('xdotool key End')
+            elif value == "del":
+                systex('xdotool key Delete')
+            elif value == "enter":
+                systex('xdotool key Return')
+            elif value == "backspace":
+                systex('xdotool key BackSpace')
+            elif value == "tab":
+                systex('xdotool key Tab')
+            elif value == "pup":
+                systex('xdotool key Page_Up')
+            elif value == "pdown":
+                press('xdotool key Page_Down')
+            elif value == "shift":
+                systex('xdotool key Shift_L')
+            elif value == "ctrl":
+                systex('xdotool key Control_L')
+            elif value == "alt":
+                systex('xdotool key Alt_L')
+            elif value == "super":
+                systex('xdotool key Super_L')
+
+        elif platform == 'windows':
             from keyboard import send as press
 
             if value == "psc":
@@ -60,8 +105,6 @@ def press_key(platform, todo, value):
                 press('home')
             elif value == "end":
                 press('end')
-            elif value == "ins":
-                press('insert')
             elif value == "del":
                 press('delete')
             elif value == "enter":
@@ -82,3 +125,6 @@ def press_key(platform, todo, value):
                 press('alt')
             elif value == "super":
                 press('left windows')
+
+        else:
+            print('Not implemented.')
--- driver/main.py
+++ driver/main.py
@@ -3,15 +3,15 @@
 def errorclose(reason, platform):
     if platform == None:
         print(f'The program encountered a problem and needs to close.\nError: “{reason}”')
-    if platform == 'windows':
-        from win10toast import ToastNotifier as toast # Solutions must be found to reduce the size of the dependencies (win32 especially).
-
-        toast().show_toast('Fatal error !', f'PiDeck driver has stopped.\nError: “{reason}”', "C:\\Firmin\\PiDeck\\logo\\PiDeck_logo.ico", 5, True) # Show notification if PiDeck needs to close because of an important error.
+    if platform == 'windows' or platform == 'macos' or platform == 'linux': # Show notification if PiDeck needs to close because of an important error.
+        from tkinter import Tk, messagebox
+        window = Tk()
+        window.withdraw()
+        messagebox.showerror('PiDeck ─ error', f'The program encountered a problem and needs to close\nError: “{reason}”')
     else:
         print(f'The program encountered a problem and needs to close.\nError: “{reason}”')
 
 try:
-    import change_volume as vol
     import prog_init as init
     from os import system as systex
 
@@ -20,7 +20,12 @@ try:
         systex('python wsgi_waitress.py') # Run the HTTP server.
         print('win')
     elif platform == 'macos':
+        errorclose(reason="macOS is not supported for now.", platform=platform)
+        """
+        THIS PART OF THE PROGRAM IS DISABLED AS MANY THINGS DON'T WORK ON macOS.
+
         systex("gunicorn --certfile cert.pem --keyfile key.pem --bind 0.0.0.0:9876 wsgi_gunicorn:app") # Run the HTTPS server.
+        """
     elif platform == 'linux':
         pc_init = init.init_lnx() # Run the function to initialize the program.
         systex("gunicorn --certfile cert.pem --keyfile key.pem --bind 0.0.0.0:9876 wsgi_gunicorn:app") # Run the HTTPS server.
