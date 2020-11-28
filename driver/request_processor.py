926e3c64d864b2b04072f9a4d82d21f980ffd7c7
A-delta <tvn.louis@gmail.com>
2020-11-28 19:57:19 +0100

    Template proposition for pin.function link.

--- driver/request_processor.py
+++ driver/request_processor.py
@@ -1,14 +1,26 @@
+config = [
+    {"pin": 21, "function": "example_function"},
+    {"pin": 20, "function": "example_function2"}
+]
+
+
 def process(json):
     print("Processing...")
 
     if "inventory" in json.keys():
         makeInventory(json)
     else:
-        execute(json)
+        find_associate_function(json)
 
 def makeInventory(json):
     print("Got inventory :", json)
 
-def execute(json):
-    print("Executing request :", json)
 
+def example_function(json):
+    print("I'm an example.")
+
+def example_function2(json):
+    print("I'm another example.")
+
+def find_associate_function(json):
+    print("I'm useless for now")
