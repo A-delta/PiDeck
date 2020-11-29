6d42c7c59b88c17ee4e0016d9a4d804d5d2d8442
A-delta <tvn.louis@gmail.com>
2020-11-29 14:45:54 +0100

    im stupid

--- pi/raspimote/pi.py
+++ pi/raspimote/pi.py
@@ -130,11 +130,11 @@ class Pi:
 
                 for channel in range(self.ADC_channels + 1):
                     old = self.ADC_old_values[channel]
-                    new = round(int(self.ADC.analogRead(channel)))
+                    new = int(self.ADC.analogRead(channel))
 
                     if old not in [new-2, new-1, new, new+1, new+2]:
 
-                        new = (new/255)*100
+                        new = int(new/255)*100
                         idle = 1
                         time_sleep = 0.1
 
