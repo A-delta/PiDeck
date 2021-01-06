15a1e6c0dc79007c363c038f70ea26642d185fdb
A-delta <tvn.louis@gmail.com>
2021-01-06 22:18:31 +0100

    Created script to identify usb device in /dev/input/event*

---
+++ pi/identify_usb_device.py
@@ -0,0 +1,27 @@
+# 2021 Adelta
+# https://github.com/A-delta
+
+from os import chdir, listdir
+from threading import Thread
+from evdev import InputDevice, ecodes
+
+
+def find_device(event_number):
+    device = InputDevice(f"/dev/input/{event_number}")
+    for event in device.read_loop():
+        if event.type == ecodes.EV_KEY:
+            print(f"/dev/input/{event_number}")
+
+def main():
+    chdir("/dev/input")
+    devices = []
+    for e in listdir():
+        if "event" in e:
+            devices.append(e)
+
+    for d in devices:
+        t = Thread(target=find_device, args=[d])
+        t.start()
+
+if __name__ == "__main__":
+    main()
