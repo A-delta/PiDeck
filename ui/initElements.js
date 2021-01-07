ebae0f364b9093bf29a7dfb2df923b37dc208c2a
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2021-01-07 08:03:00 +0100

    UI - Fixed <option> appears

--- ui/initElements.js
+++ ui/initElements.js
@@ -1,10 +1,10 @@
 function init() {
-    var items = ["", "GPIO_13", "ADC_1"];
+    var items = ["", "GPIO 13", "ADC 1"];
     var items_id = [];
     k = 0;
     for (var item of items) {
         if (k != 0) {
-            items_id.push(item.toLowerCase())
+            items_id.push(item.toLowerCase().replace(" ", "_"))
         }
         else {
             items_id.push("none")
@@ -15,10 +15,10 @@ function init() {
     k = 0;
     for (var item of items) {
         opt_id = items_id[k];
-        str += '<option id="' + opt_id + '">' + item + "</option>";
+        str += '<option value="' + opt_id + '">' + item + "</option>";
         k += 1;
     }
-    console.log(str);
+
     document.getElementById("element").innerHTML = str;
 }
 
