15bd3e2b89aa173b16eae9d9c01a1c280232b940
A-delta <tvn.louis@gmail.com>
2021-01-20 11:05:10 +0100

    Investigating

--- pi/raspimote/server_pi/stop_server.sh
+++ pi/raspimote/server_pi/stop_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 Port=9876
 pid=`ps ax | grep gunicorn | grep $Port | awk '{split($0,a," "); print a[1]}' | head -n 1`
-if [ -z "$pid" ]; then
+if [ -z "$pid" ];
+then
   echo "no gunicorn deamon on port $Port"
 else
   kill $pid
