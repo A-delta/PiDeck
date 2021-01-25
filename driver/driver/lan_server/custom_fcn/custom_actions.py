d01b63c74bee096c138705185a87eaa6ebea31c6
Firmin Launay <firmin.launay@outlook.fr>
2021-01-25 15:28:44 +0100

    Update custom functions file

---
+++ driver/driver/lan_server/custom_fcn/custom_actions.py
@@ -0,0 +1,5 @@
+"""
+Below this text, you should add all the functions you created (simply declare them with "def()").
+Don't forget the imports at the beginning of the file!
+If you think that some functions should come built-in, don't hesitate to share them with us!
+"""
