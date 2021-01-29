0dbbee830111ff82e009dcf3b7dc010cb32b90fb
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-01-29 16:29:06 +0100

    Update batch (Windows)

--- install/win_assets/init.cmd
+++ install/win_assets/init.cmd
@@ -2,4 +2,4 @@
 
 cd "C:\Program Files\RaspiMote\driver"
 
-Rem "C:\Program Files\RaspiMote\py\pythonw.exe" main.py   #   Disabled for the moment.
+"C:\Program Files\RaspiMote\py\python.exe" main.py
