5fc8c204b8656e912908843513b931279731b621
A-delta <tvn.louis@gmail.com>
2021-01-30 19:23:34 +0100

    Testing handling only ConnectionRefusedError

--- pi/raspimote/pi.py
+++ pi/raspimote/pi.py
@@ -174,7 +174,7 @@ class Pi:
             content = dumps({"code": self.code, "request": {"type": "ping"}})
             try:
                 response = post(self.server_url, data=content, headers=self.request_headers, verify=False)
-            except ConnectionError or ConnectionResetError:
+            except ConnectionRefusedError:
                 self.log("Timeout! Restarting connection procedure")
                 break
         print("I ended")
