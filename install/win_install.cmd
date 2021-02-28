c6a14013b995b1ae8df99c5e3cd576b02425f604
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-02-28 17:55:00 +0100

    Update Python to 3.9.2 & start to implement better way to run at startup

--- install/win_install.cmd
+++ install/win_install.cmd
@@ -50,7 +50,7 @@ goto check_Permissions
 
     echo [[92mv[0m] RaspiMote code copied to installation folder.
 
-    curl -s -L -o "C:\Users\%USERNAME%\Downloads\python_embeddable.zip" "https://www.python.org/ftp/python/3.9.1/python-3.9.1-embed-amd64.zip"
+    curl -s -L -o "C:\Users\%USERNAME%\Downloads\python_embeddable.zip" "https://www.python.org/ftp/python/3.9.2/python-3.9.2-embed-amd64.zip"
 
     echo [[92mv[0m] Python embeddable environment downloaded.
 
