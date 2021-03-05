2d217c9cf515bfd9ebe22c2a4963a9ad0924d959
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-03-05 08:12:07 +0100

    Update wsgi

--- driver/driver/lan_server/wsgi_cheroot.py
+++ driver/driver/lan_server/wsgi_cheroot.py
@@ -4,12 +4,12 @@ from cheroot.ssl.builtin import BuiltinSSLAdapter
 
 from lan_server import app
 
-my_app = WSGIPathInfoDispatcher({'/': app})
-server = WSGIServer(('0.0.0.0', 9876), my_app)
+my_app = WSGIPathInfoDispatcher({'/': app}, verbose=True)
+server = WSGIServer(('0.0.0.0', 9876), my_app, verbose=True)
 
 ssl_cert = "cert.pem"
 ssl_key = "key.key"
-server.ssl_adapter =  BuiltinSSLAdapter(ssl_cert, ssl_key, None)
+server.ssl_adapter =  BuiltinSSLAdapter(ssl_cert, ssl_key, verbose=True)
 
 if __name__ == '__main__':
    try:
