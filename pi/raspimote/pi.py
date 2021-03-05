5d118d389ffcf52d810a78e5aafd641db3a05950
A-delta <louis.tvnt@gmail.com>
2021-03-05 20:58:32 +0100

    value -> val

--- pi/raspimote/pi.py
+++ pi/raspimote/pi.py
@@ -309,7 +309,7 @@ class Pi:
         })
 
     def on_trigger_moved(self, trigger):
-        self.log(f"Trigger pushed to {trigger.value}")
+        self.log(f"Trigger pushed to {trigger.val}")
         self.send_data({
             "code": self.code,
 
@@ -317,7 +317,7 @@ class Pi:
                 "type": "Gamepad",
                 "pin": '0',
                 "value": trigger.name,
-                "extra": round(trigger.value, 2)
+                "extra": round(trigger.val, 2)
             }
 
         })
