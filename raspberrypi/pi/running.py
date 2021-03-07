0da639ea6fc093ffba55a888a63bc4ab442c437a
A-delta <louis.tvnt@gmail.com>
2021-03-07 20:32:48 +0100

    smal fix

--- raspberrypi/pi/running.py
+++ raspberrypi/pi/running.py
@@ -79,7 +79,7 @@ class Mixin:
         else:
             start = 0
 
-        if not data["request"]["type"] == "inventory":
+        if type(data) == dict:
 
             data = {"code": self.code,
                     "request":
