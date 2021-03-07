19f9e7cc89e2a9730597b1e8e3a5d04c3da3efe4
A-delta <louis.tvnt@gmail.com>
2021-03-07 20:36:04 +0100

    Testing new code structure

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -14,8 +14,9 @@ def main(argv):
 
     pi = Pi("192.168.1.16", "WiFi", verbose)
 
-    pi.add_generic_USB_device(0, "mouseblue")
-    pi.add_generic_USB_device(1, "numpad")
+    pi.add_USB_mouse(0)
+    pi.add_USB_keyboard(1)
+    pi.add_gamepad_device(0)
 
     pi.establish_connection()
 
